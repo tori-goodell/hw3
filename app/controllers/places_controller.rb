8cd298ae6e46d82ac2a7e7d84aa2a72be15495aa
tori-goodell <tori.goodell@kellogg.northwestern.edu>
2022-05-15 17:45:59 +0000

    updating places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,24 @@
 class PlacesController < ApplicationController
 
     def index 
-        @place = params["place"]
+        @places = Place.all
     end
+
+    def show
+        @place = Place.find_by({"id" => params["id"]})
+        @posts = Post.where({ "place_id" => @place["id"]})
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place["description"] = params["place"]["description"]
+        @place["state"] = params["place"]["posted-on"]
+        @place.save
+        redirect_to "/places"
+      end
 end
