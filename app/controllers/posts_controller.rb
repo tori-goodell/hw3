8dc8e63f2cc0fbc5dc56e569e608a6a2dec333e7
tori-goodell <tori.goodell@kellogg.northwestern.edu>
2022-05-16 23:30:53 +0000

    polished up code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,5 @@
 class PostsController < ApplicationController
 
-    def show
-        @post = Post.find_by({ "id" => params["id"] })
-        @place = Place.find_by({ "id" => @post["place_id"] })
-    end
-
     def new
         @post = Post.new
         @place = Place.find_by({ "id" => params["place_id"] })
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  get("/", { :controller => "places", :action => "index" })
 end
